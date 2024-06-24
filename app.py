00260ca643e2bee540398710012c18e000634e84
Sarowar Jahan Saurav <64561923+SarowarSaurav@users.noreply.github.com>
2024-06-24 17:51:34 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from fastapi import FastAPI
-import gradio as gr
+import gradio as gr 
 
-app = FastAPI()
+from transformers import pipeline 
 
-with gr.Blocks() as demo:
-    gr.Markdown("Hi friends!")
+ 
 
-app = gr.mount_gradio_app(app, demo, path="/")
+classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True) 
+
+ 
+
+def detect_emotions(emotion_input): 
+
+  prediction = classifier(emotion_input,) 
+
+  output = {} 
+
+  for emotion in prediction[0]: 
+
+    output[emotion["label"]] = emotion["score"] 
+
+  return output 
+
+ 
+
+examples = [["I am excited to announce that I have been promoted"], ["Sorry for the late reply"]] 
+
+ 
+
+demo = gr.Interface(fn=detect_emotions, inputs=gr.Textbox(placeholder="Enter text here", label="Input"), outputs=gr.Label(label="Emotion"), examples=examples) 
+
+demo.launch(server_name="0.0.0.0", server_port=7000) 
